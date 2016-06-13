9aefe25929c12d9a6815ce973adea431ce285015
wwagner4 <wwagner4@gmail.com>
2016-06-13 07:55:53 +0200

    rectangular background ready

--- doctuswebaudiotryout-core/src/main/scala/net/entelijan/DoctuswebaudiotryoutTemplate.scala
+++ doctuswebaudiotryout-core/src/main/scala/net/entelijan/DoctuswebaudiotryoutTemplate.scala
@@ -5,45 +5,20 @@ import doctus.core.util._
 import doctus.core.template._
 import doctus.core.color._
 
-case class WordPos(word: String, x: Double, y: Double, size: Double)
-
-/**
- * In here comes the complete logic for your project
- */
 case class DoctuswebaudiotryoutDoctusTemplate(canvas: DoctusCanvas) extends DoctusTemplate {
 
-  val ran = new java.util.Random
-
-  val mx = canvas.width / 2.0 - 80
-  val my = canvas.height / 2.0
-  val wordList = List("here", "comes", "your", "creativity")
-  var words = wordList.map(word => WordPos(word, mx, my, 50))
 
   def draw(g: DoctusGraphics): Unit = {
     val w = canvas.width
     val h = canvas.height
-
-    def drawBackground(g: DoctusGraphics): Unit = {
-      g.fill(DoctusColorBlack, 30)
-      g.rect(DoctusPoint(0, 0), w, h)
+    g.fill(DoctusColorYellow, 255)
+    val nx = 3
+    val ny = 4
+    val dx = w.toDouble / nx
+    val dy = h.toDouble / ny
+    for (i <- 0 until nx; j <- 0 until ny) {
+      g.rect(i * dx, j * dy, dx, dy)
     }
-
-    def drawWord(wp: WordPos): Unit = {
-      g.textSize(wp.size)
-      g.text(wp.word, DoctusPoint(wp.x, wp.y), 0)
-    }
-
-    def next(l: WordPos): WordPos = {
-      val d1 = ran.nextInt(23) - 11
-      val d2 = ran.nextInt(17) - 8
-      val d3 = ran.nextInt(7) - 3
-      WordPos(l.word, l.x + d1, l.y + d2, l.size + d3)
-    }
-
-    drawBackground(g)
-    g.fill(DoctusColorWhite, 255)
-    words = words.map(next)
-    words.foreach(drawWord)
   }
 
   def pointableDragged(pos: DoctusPoint): Unit = () // Nothing to do here
