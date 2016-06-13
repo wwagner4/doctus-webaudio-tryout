e141333fb8f72a9c976b36013293376a5dd82080
wwagner4 <wwagner4@gmail.com>
2016-06-13 22:16:15 +0200

    can start and stop one note

--- doctuswebaudiotryout-scalajs/src/main/scala/doctus/sound/DoctusSoundJs.scala
+++ doctuswebaudiotryout-scalajs/src/main/scala/doctus/sound/DoctusSoundJs.scala
@@ -1,12 +1,29 @@
 package doctus.sound
 
+import java.time.Period
+
 /**
   * ScalaJS implementation of the (experimental sound interface)
   */
 class DoctusSoundJs extends DoctusSound {
 
-  override def noteOn: Unit = println("noteOn")
+  import org.scalajs.dom.{AudioBuffer, Event, XMLHttpRequest, GainNode, AudioContext}
 
-  override def noteOff: Unit = println("noteOff")
+  val ctx = new AudioContext
+
+  val oscil = ctx.createOscillator()
+  oscil.frequency.value = 444
+
+  override def noteOn: Unit = {
+    println("noteOn")
+    oscil.connect(ctx.destination)
+    oscil.start()
+  }
+
+  override def noteOff: Unit = {
+    println("noteOff")
+    oscil.stop()
+    oscil.disconnect(ctx.destination)
+  }
 
 }
