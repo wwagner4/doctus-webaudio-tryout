2d254e8dfbe74b6c60609aeeefc632d75cf67d27
wwagner4 <wwagner4@gmail.com>
2016-06-13 08:08:25 +0200

    color stream provided

--- doctuswebaudiotryout-core/src/main/scala/net/entelijan/DoctuswebaudiotryoutTemplate.scala
+++ doctuswebaudiotryout-core/src/main/scala/net/entelijan/DoctuswebaudiotryoutTemplate.scala
@@ -8,17 +8,33 @@ import doctus.core.color._
 case class DoctuswebaudiotryoutDoctusTemplate(canvas: DoctusCanvas) extends DoctusTemplate {
 
 
+  override def frameRate = None
+
   def draw(g: DoctusGraphics): Unit = {
     val w = canvas.width
     val h = canvas.height
-    g.fill(DoctusColorYellow, 255)
-    val nx = 3
+    val nx = 4
     val ny = 4
     val dx = w.toDouble / nx
     val dy = h.toDouble / ny
+    val colorsStream = colors
+    var colorIndex = 0
+    g.noStroke()
     for (i <- 0 until nx; j <- 0 until ny) {
+      g.fill(colorsStream(colorIndex), 255)
       g.rect(i * dx, j * dy, dx, dy)
+      colorIndex += 1
     }
+  }
+
+
+  def colors: Stream[DoctusColor] = {
+    def ranColor: DoctusColor = {
+      val hue = math.random * 365
+      val (r, g, b) = DoctusColorUtil.hsv2rgb(hue.toInt, 50, 100)
+      DoctusColorRgb(r, g, b)
+    }
+    Stream.iterate(ranColor)(_ => ranColor)
   }
 
   def pointableDragged(pos: DoctusPoint): Unit = () // Nothing to do here
