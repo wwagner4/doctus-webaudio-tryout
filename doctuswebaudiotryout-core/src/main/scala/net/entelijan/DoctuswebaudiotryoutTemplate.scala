1feedb8e31969b11f160f6df150726fc5c045a74
wwagner4 <wwagner4@gmail.com>
2016-06-13 08:24:42 +0200

    refactoring

--- doctuswebaudiotryout-core/src/main/scala/net/entelijan/DoctuswebaudiotryoutTemplate.scala
+++ doctuswebaudiotryout-core/src/main/scala/net/entelijan/DoctuswebaudiotryoutTemplate.scala
@@ -7,38 +7,37 @@ import doctus.core.color._
 
 case class DoctuswebaudiotryoutDoctusTemplate(canvas: DoctusCanvas) extends DoctusTemplate {
 
+  val nx = 4
+  val ny = 4
 
   override def frameRate = None
 
   def draw(g: DoctusGraphics): Unit = {
     val w = canvas.width
     val h = canvas.height
-    val nx = 4
-    val ny = 4
     val dx = w.toDouble / nx
     val dy = h.toDouble / ny
     val colorsStream = colors
-    var colorIndex = 0
+    var cnt = 0
     g.noStroke()
     for (i <- 0 until nx; j <- 0 until ny) {
-      g.fill(colorsStream(colorIndex), 255)
+      g.fill(colorsStream(cnt), 255)
       g.rect(i * dx, j * dy, dx, dy)
-      colorIndex += 1
+      cnt += 1
     }
   }
-
 
   def colors: Stream[DoctusColor] = {
     def nextColor(c: DoctusColor): DoctusColor = {
       val (r1, g1, b1) = c.rgb
-      val (h, s, v) = DoctusColorUtil.rgb2hsv(r1, g1, b1)
-      val hue = (h + 30) % 360
-      val (r, g, b) = DoctusColorUtil.hsv2rgb(hue, s, v)
-      DoctusColorRgb(r, g, b)
+      val (h1, s1, v1) = DoctusColorUtil.rgb2hsv(r1, g1, b1)
+      val h2 = (h1 + 30) % 360
+      val (r2, g2, b2) = DoctusColorUtil.hsv2rgb(h2, s1, v1)
+      DoctusColorRgb(r2, g2, b2)
     }
     val (sr, sg, sb) = DoctusColorUtil.hsv2rgb(122, 100, 50)
     val start: DoctusColor = DoctusColorRgb(sr, sg, sb)
-    Stream.iterate(start)(c => nextColor(c))
+    Stream.iterate(start)(nextColor)
   }
 
   def pointableDragged(pos: DoctusPoint): Unit = () // Nothing to do here
