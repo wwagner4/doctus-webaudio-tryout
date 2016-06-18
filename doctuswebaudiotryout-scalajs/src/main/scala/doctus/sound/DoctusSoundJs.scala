b5dc8ee4387c7a01115c12707683b2f56ef1df9a
wwagner4 <wwagner4@gmail.com>
2016-06-18 23:48:54 +0200

    added the noise class

--- doctuswebaudiotryout-scalajs/src/main/scala/doctus/sound/DoctusSoundJs.scala
+++ doctuswebaudiotryout-scalajs/src/main/scala/doctus/sound/DoctusSoundJs.scala
@@ -1,6 +1,6 @@
 package doctus.sound
 
-import net.entelijan.{Melody, Tinitus}
+import net.entelijan.{Melody, Noise, Tinitus}
 
 /**
   * ScalaJS implementation of the (experimental sound interface)
@@ -13,13 +13,15 @@ class DoctusSoundJs extends DoctusSound {
 
   val tinitus = Tinitus(ctx)
 
+  val noise = Noise(ctx)
+
   override def tinitusStart: Unit = tinitus.start
 
   override def tinitusStop: Unit = tinitus.stop
 
-  override def noiseStart: Unit = ???
+  override def noiseStart: Unit = noise.start(ctx.currentTime)
 
-  override def noiseStop: Unit = ???
+  override def noiseStop: Unit = noise.stop(ctx.currentTime)
 
   override def melodyStart: Unit = Melody(ctx, ctx.currentTime).start()
 
---
+++ doctuswebaudiotryout-scalajs/src/main/scala/net/entelijan/Noise.scala
@@ -0,0 +1,18 @@
+package net.entelijan
+
+import org.scalajs.dom.AudioContext
+
+/**
+  * Creating some noise.
+  */
+case class Noise(ctx: AudioContext) {
+
+  def start(time: Double): Unit = {
+
+  }
+
+  def stop(time: Double): Unit = {
+
+  }
+
+}
