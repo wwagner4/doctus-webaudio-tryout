dd49ae3c8d009a26278662181e21478ef6efb84a
wwagner4 <wwagner4@gmail.com>
2016-06-18 10:52:06 +0200

    melody works quite well

--- doctuswebaudiotryout-scalajs/src/main/scala/net/entelijan/Melody.scala
+++ doctuswebaudiotryout-scalajs/src/main/scala/net/entelijan/Melody.scala
@@ -10,15 +10,25 @@ import org.scalajs.dom.AudioContext
   */
 case class Melody(ctx: AudioContext, now: Double) {
 
+  val freqs = List(111, 222, 333, 444, 555, 666, 777)
+  val ran = new java.util.Random()
+
   def start(): Unit = {
-    playNote(0, 2, MyInstrument(ctx, 444))
-    playNote(1, 2, MyInstrument(ctx, 555))
-    playNote(3, 2, MyInstrument(ctx, 222))
+    for (t <- 0.0 to (16, 0.3)) {
+      val i = ran.nextInt(freqs.size)
+      if (ranBoolean(0.7)) {
+        playNote(t, 0.5, MyInstrument(ctx, freqs(i)))
+      }
+    }
+  }
+
+  private def ranBoolean(p: Double): Boolean = {
+    return ran.nextDouble() < p
   }
 
   private def playNote(time: Double, duration: Double, inst: Instrument): Unit = {
     inst.start(now + time)
-    inst.start(now + time + duration)
+    inst.stop(now + time + duration)
   }
 
 }
@@ -45,11 +55,11 @@ case class MyInstrument(ctx: AudioContext, freq: Double) extends Instrument {
 
   override def start(time: Double): Unit = {
     gain.gain.setValueAtTime(0, time)
-    gain.gain.linearRampToValueAtTime(1.0, 2.0)
+    gain.gain.linearRampToValueAtTime(1.0, time + 0.01)
   }
 
   override def stop(time: Double): Unit = {
     gain.gain.setValueAtTime(1.0, time)
-    gain.gain.linearRampToValueAtTime(0.0, time + 2.0)
+    gain.gain.linearRampToValueAtTime(0.0, time + 1.0)
   }
 }
