5d854fa741984951869c90cfd4af9555edeef155
wwagner4 <wwagner4@gmail.com>
2016-06-19 07:49:52 +0200

    refactored creation of noise buffer

--- doctuswebaudiotryout-scalajs/src/main/scala/net/entelijan/Noise.scala
+++ doctuswebaudiotryout-scalajs/src/main/scala/net/entelijan/Noise.scala
@@ -1,6 +1,6 @@
 package net.entelijan
 
-import org.scalajs.dom.{AudioBuffer, AudioBufferSourceNode, AudioContext}
+import org.scalajs.dom.{AudioBuffer, AudioBufferSourceNode, AudioContext, AudioNode}
 
 import scala.util.Random
 
@@ -11,14 +11,12 @@ case class Noise(ctx: AudioContext) {
 
   val ran = Random
 
-  val buffer = createBuffer
+  val buffer = WebAudioUtil.createBufferNoise(ctx, ran, 0.3)
 
   var bufferSrcOpt = Option.empty[AudioBufferSourceNode]
 
   def start(time: Double): Unit = {
-    val bufferSrc = ctx.createBufferSource()
-    bufferSrc.buffer = buffer
-    bufferSrc.loop = true
+    val bufferSrc = WebAudioUtil.createBufferSourceLooping(ctx, buffer)
     bufferSrc.connect(ctx.destination)
     bufferSrc.start()
     bufferSrcOpt = Some(bufferSrc)
@@ -26,22 +24,33 @@ case class Noise(ctx: AudioContext) {
 
   def stop(time: Double): Unit = {
     bufferSrcOpt.foreach { bufferSrc =>
-      bufferSrc.connect(ctx.destination)
+      bufferSrc.disconnect(ctx.destination)
       bufferSrc.stop()
     }
   }
 
-  private def createBuffer: AudioBuffer = {
-    val ampl = 0.5f
+}
+
+object WebAudioUtil {
+
+  def createBufferNoise(ctx: AudioContext, ran: Random, gain: Double): AudioBuffer = {
     val bufferSize = 2 * ctx.sampleRate.toInt
     val buffer = ctx.createBuffer(1, bufferSize, ctx.sampleRate.toInt)
     val channel = buffer.getChannelData(0)
     for (i <- 0 until bufferSize) {
-      val v = ran.nextFloat() * 2 * ampl - ampl
+      val v = (ran.nextFloat() * 2 - 1) * gain.toFloat
       channel.set(i, v)
     }
     buffer
   }
+
+  def createBufferSourceLooping(ctx: AudioContext, buffer: AudioBuffer): AudioBufferSourceNode = {
+    val bufferSrc = ctx.createBufferSource()
+    bufferSrc.buffer = buffer
+    bufferSrc.loop = true
+    bufferSrc
+  }
+
 
 }
 
