c464cf7cb7f21c90586e0c54bddf3de3747d2ef9
wwagner4 <wwagner4@gmail.com>
2016-06-20 08:13:11 +0200

    renaming. offset not yet OK

--- doctuswebaudiotryout-scalajs/src/main/scala/net/entelijan/Noise.scala
+++ doctuswebaudiotryout-scalajs/src/main/scala/net/entelijan/Noise.scala
@@ -1,9 +1,7 @@
 package net.entelijan
 
-import org.scalajs.dom.raw.{AudioNode, AudioParam}
 import org.scalajs.dom.{AudioBuffer, AudioBufferSourceNode, AudioContext, AudioNode}
 
-import scala.scalajs.js
 import scala.util.Random
 
 /**
@@ -11,21 +9,20 @@ import scala.util.Random
   */
 case class Noise(ctx: AudioContext) {
 
-  val buffer = WebAudioUtil.createBufferNoise(ctx, Random)
-  val gain = ctx.createGain()
-  gain.gain.value = 1
-  val gainMod = WebAudioUtil.createNodeLFO(ctx, 10, 0.5, 3)
-  gainMod.start(0)
-  gainMod.output.connect(gain.gain)
-  gain.connect(ctx.destination)
+  val noiseBuffer = WebAudioUtil.createBufferNoise(ctx, Random)
+  val noiseGain = ctx.createGain()
+  val noiseGainMod = WebAudioUtil.createNodeLFO(ctx, 10, 0.3, 0.0)
+  noiseGainMod.start(0)
+  noiseGainMod.output.connect(noiseGain.gain)
+  noiseGain.connect(ctx.destination)
 
   var bufferSrcOpt = Option.empty[AudioBufferSourceNode]
 
   def start(time: Double): Unit = {
-    val bufferSrc = WebAudioUtil.createBufferSourceLooping(ctx, buffer)
-    bufferSrc.connect(gain)
-    bufferSrc.start()
-    bufferSrcOpt = Some(bufferSrc)
+    val noiseNode = WebAudioUtil.createBufferSourceLooping(ctx, noiseBuffer)
+    noiseNode.connect(noiseGain)
+    noiseNode.start()
+    bufferSrcOpt = Some(noiseNode)
   }
 
   def stop(time: Double): Unit = {
@@ -70,33 +67,34 @@ object WebAudioUtil {
   }
 
   def createNodeLFO(ctx: AudioContext, frequency: Double, amplitude: Double, offset: Double): CustomSourceNode = {
-    val oscil = ctx.createOscillator()
-    oscil.frequency.value = frequency
+    val lfo = ctx.createOscillator()
+    lfo.frequency.value = frequency
 
-    val gain = ctx.createGain()
-    gain.gain.value = amplitude
+    val lfoGain = ctx.createGain()
+    lfoGain.gain.value = amplitude
 
     val offsetBuffer = createBufferConstant(ctx, offset)
     val offsetNode = createBufferSourceLooping(ctx, offsetBuffer)
 
-    val mixer = ctx.createChannelMerger(2)
+    val mixer = ctx.createChannelMerger()
 
-    oscil.connect(gain)
+    lfo.connect(lfoGain)
 
     offsetNode.connect(mixer)
-    gain.connect(mixer)
+    lfoGain.connect(mixer)
 
     new CustomSourceNode {
 
       override def output: AudioNode = mixer
 
-      override def stop(time: Double): Unit = {
-        oscil.stop(time)
+      override def start(time: Double): Unit = {
+        lfo.start(time)
       }
 
-      override def start(time: Double): Unit = {
-        oscil.start(time)
+      override def stop(time: Double): Unit = {
+        lfo.stop(time)
       }
+
     }
 
   }
