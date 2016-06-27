07635e5ad5a31f37d856d3dfc04ca0cf56706e7b
wwagner4 <wwagner4@gmail.com>
2016-06-27 19:53:25 +0200

    Added comments

--- doctuswebaudiotryout-core/src/test/scala/net/entelijan/PinkNoise.scala
+++ doctuswebaudiotryout-core/src/test/scala/net/entelijan/PinkNoise.scala
@@ -1,5 +1,15 @@
 package net.entelijan
 
+/**
+  * http://sampo.kapsi.fi/PinkNoise/PinkNoise.java
+  *
+  * Copyright (c) 2008, Sampo Niskanen <sampo.niskanen@iki.fi>
+  * All rights reserved.
+  * Source:  http://www.iki.fi/sampo.niskanen/PinkNoise/
+  *
+  * Distrubuted under the BSD license:
+  *
+  */
 class PinkNoise {
 
 }
