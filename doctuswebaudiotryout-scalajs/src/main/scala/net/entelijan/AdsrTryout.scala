f2b8b4547ad5838f2d8176ea3e549c506e123cf1
wwagner4 <wwagner4@gmail.com>
2016-07-05 22:39:51 +0200

    ads demo working quite well

--- doctuswebaudiotryout-scalajs/src/main/scala/net/entelijan/AdsrTryout.scala
+++ doctuswebaudiotryout-scalajs/src/main/scala/net/entelijan/AdsrTryout.scala
@@ -1,17 +1,24 @@
 package net.entelijan
-import doctus.sound.Nineth
+
+import doctus.sound._
 import org.scalajs.dom.{AudioContext, AudioNode}
+
+import scala.util.Random
 
 /**
   * Sound using a reusable ADSR Curve
   */
 case class AdsrTryout(ctx: AudioContext) {
 
+  val freqs = List(111, 222, 333, 444, 555)
+  val ran = Random
+
   val oscil = ctx.createOscillator()
   oscil.`type` = "sawtooth"
-  oscil.frequency.value = 222f
+  oscil.frequency.value = 0f
 
   val adsr = Adsr(ctx)
+  adsr.release = 2.0
 
   oscil.connect(adsr.in)
   adsr.out.connect(ctx.destination)
@@ -19,8 +26,39 @@ case class AdsrTryout(ctx: AudioContext) {
   oscil.start()
 
   def start(nineth: Nineth): Unit = {
-    println("start " + nineth)
-    // TODO Use nineth here to control the adsr
+    val i = ran.nextInt(freqs.size)
+    oscil.frequency.value = freqs(i)
+    nineth match {
+      case N_00 =>
+        adsr.attack = 0.001
+        adsr.sustain = 0.3
+      case N_10 =>
+        adsr.attack = 0.01
+        adsr.sustain = 0.3
+      case N_20 =>
+        adsr.attack = 0.6
+        adsr.sustain = 0.3
+
+      case N_01 =>
+        adsr.attack = 0.001
+        adsr.sustain = 0.1
+      case N_11 =>
+        adsr.attack = 0.01
+        adsr.sustain = 0.1
+      case N_21 =>
+        adsr.attack = 0.6
+        adsr.sustain = 0.1
+
+      case N_02 =>
+        adsr.attack = 0.001
+        adsr.sustain = 0.01
+      case N_12 =>
+        adsr.attack = 0.01
+        adsr.sustain = 0.01
+      case N_22 =>
+        adsr.attack = 0.6
+        adsr.sustain = 0.01
+    }
     adsr.start(ctx.currentTime)
   }
 
@@ -32,10 +70,10 @@ case class AdsrTryout(ctx: AudioContext) {
 
 case class Adsr(ctx: AudioContext) extends CustomNode {
 
-  val attack = 0.01
-  val decay = 0.1
-  val sustain = 0.01
-  val release = 0.5
+  var attack = 0.01
+  var decay = 0.1
+  var sustain = 0.01
+  var release = 0.5
 
   val gain = ctx.createGain()
   gain.gain.setValueAtTime(0, 0)
