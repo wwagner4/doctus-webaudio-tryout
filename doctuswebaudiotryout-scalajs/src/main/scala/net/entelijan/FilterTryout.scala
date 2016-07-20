896f2ef917c3ceda28a9cfd8c2a8e4e6bea0d851
wwagner4 <wwagner4@gmail.com>
2016-07-20 07:25:42 +0200

    connected filter detune but does not yet work

--- doctuswebaudiotryout-scalajs/src/main/scala/net/entelijan/FilterTryout.scala
+++ doctuswebaudiotryout-scalajs/src/main/scala/net/entelijan/FilterTryout.scala
@@ -7,22 +7,42 @@ import org.scalajs.dom._
 
 case class FilterTryout(ctx: AudioContext) {
 
+  val freq = 555.0
+
   val oscil = ctx.createOscillator()
   oscil.`type` = "sawtooth"
   oscil.start(0)
   val adsr = NodeAdsr(ctx)
 
-  oscil.connect(adsr.nodeIn)
+  val filterAdsr = NodeAdsr(ctx)
+  filterAdsr.valAttack = 1.0
+  filterAdsr.valSustain = 1.0
+  filterAdsr.valDecay = 1.0
+
+  val filterGain = ctx.createGain()
+  filterGain.gain.value = 1.0
+
+  val filter = ctx.createBiquadFilter()
+  filter.`type` = "lowpass"
+  filter.frequency.value = freq * 1.3
+
+  oscil.connect(filter)
+  filter.connect(adsr.nodeIn)
   adsr.nodeOut.connect(ctx.destination)
+
+  filterAdsr.nodeOut.connect(filterGain)
+  filterGain.connect(filter.detune)
 
   def start(): Unit = {
     val t = ctx.currentTime
     adsr.start(t)
+    filterAdsr.start(t)
   }
 
   def stop(): Unit = {
     val t = ctx.currentTime
     adsr.stop(t)
+    filterAdsr.stop(t)
   }
 
 }
