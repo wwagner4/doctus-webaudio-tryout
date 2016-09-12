d88833a7e76187cc3de52b807a71dcd222e5765e
wwagner4 <wwagner4@gmail.com>
2016-09-12 23:39:08 +0200

    changed params

--- doctuswebaudiotryout-scalajs/src/main/scala/net/entelijan/Melody.scala
+++ doctuswebaudiotryout-scalajs/src/main/scala/net/entelijan/Melody.scala
@@ -53,7 +53,7 @@ trait Instrument {
 case class MyInstrument(ctx: DoctusSoundAudioContext, freq: Double) extends Instrument {
 
   val freqCtrl = ctx.createNodeControlConstant(freq)
-  val adsrCtrl = ctx.createNodeControlAdsr(0.1, 0.2, 0.3, 3.0)
+  val adsrCtrl = ctx.createNodeControlAdsr(0.001, 0.1, 0.2, 3.0)
 
   val oscil = ctx.createNodeSourceOscil(WaveType_Sawtooth)
   val gain = ctx.createNodeFilterGain
