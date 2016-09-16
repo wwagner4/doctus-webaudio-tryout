1a426b8cb0f4605549ece13eb54709b2d6b365e9
wwagner4 <wwagner4@gmail.com>
2016-09-16 08:26:32 +0200

    parameters in metal work fine

--- doctuswebaudiotryout-scalajs/src/main/scala/net/entelijan/MetalTryout.scala
+++ doctuswebaudiotryout-scalajs/src/main/scala/net/entelijan/MetalTryout.scala
@@ -10,14 +10,26 @@ case class MetalTryout(ctx: DoctusSoundAudioContext) {
   val ran = Random
   val baseFreqs = List(111, 222, 333, 444, 555)
 
-  var gainableOscilsOpt = Option.empty[Seq[NodeSourceOscil]]
+  var gainableOscilsOpt = Option.empty[Seq[StartStoppableSource]]
   var adsrOpt = Option.empty[NodeControlEnvelope]
+
+  case class StartStoppableSource(startStoppable: StartStoppable, nodeSource: NodeSource) extends NodeSource with StartStoppable {
+
+    def start(time: Double): Unit = startStoppable.start(time)
+
+    def stop(time: Double): Unit = startStoppable.stop(time)
+
+    def connect(sink: NodeSink): Unit = nodeSource.connect(sink)
+
+    def connect(filter: NodeFilter): NodeSource = nodeSource.connect(filter)
+
+  }
 
   def start(): Unit = {
 
     def ranFreq: Double = baseFreqs(ran.nextInt(baseFreqs.size))
     val harmonics = SoundUtil.metalHarmonics(ranFreq, 6)
-    val gains = Stream.from(0).map(SoundUtil.logarithmicDecay(3)(_))
+    val gains = Stream.from(0).map(SoundUtil.logarithmicDecay(1.4)(_)).map(_ * 0.5)
 
     val params = harmonics.zip(gains)
 
@@ -45,7 +57,8 @@ case class MetalTryout(ctx: DoctusSoundAudioContext) {
     adsrOpt.foreach(_.stop(now))
   }
 
-  def gainableOscil(freqVal: Double, gainVal: Double): NodeSourceOscil = {
+  def gainableOscil(freqVal: Double, gainVal: Double): StartStoppableSource = {
+
     val oscil = ctx.createNodeSourceOscil(WaveType_Sine)
     val gain = ctx.createNodeFilterGain
 
@@ -56,7 +69,7 @@ case class MetalTryout(ctx: DoctusSoundAudioContext) {
     gainCtrl >- gain.gain
     oscil >- gain
 
-    oscil
+    StartStoppableSource(oscil, gain)
   }
 
 }
