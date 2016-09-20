d75983f5935b2f6def4d9a789e23304fc5cb7399
wwagner4 <wwagner4@gmail.com>
2016-09-20 22:35:16 +0200

    added inst to delay

--- doctuswebaudiotryout-core/src/main/scala/net/entelijan/experiments/DelayExperiment.scala
+++ doctuswebaudiotryout-core/src/main/scala/net/entelijan/experiments/DelayExperiment.scala
@@ -3,19 +3,36 @@ package net.entelijan.experiments
 import net.entelijan.SoundExperiment
 import net.entelijan.Nineth
 import doctus.sound.DoctusSoundAudioContext
+import doctus.sound.StartStoppable
 
 case class DelayExperiment(ctx: DoctusSoundAudioContext) extends SoundExperiment {
 
   def title: String = "delay"
-
+  
+  var instOpt = Option.empty[Inst]
+  
   def start(nineth: Nineth): Unit = {
     val now = ctx.currentTime
-    println("started at %.2f" format now)
+    val inst = Inst()
+    inst.start(now)
+    instOpt = Some(inst)
   }
 
   def stop(): Unit = {
     val now = ctx.currentTime
-    println("stopped at %.2f" format now)
+    instOpt.foreach { _.stop(now) }
+  }
+
+  case class Inst() extends StartStoppable {
+    
+    def start(time: Double): Unit = {
+      println("INST started at %.2f" format time)
+    }
+
+    def stop(time: Double): Unit = {
+      println("INST stopped at %.2f" format time)
+    }
+    
   }
 
 }
