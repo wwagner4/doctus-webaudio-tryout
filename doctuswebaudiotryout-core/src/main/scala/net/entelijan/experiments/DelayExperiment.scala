44a146f934c13b9ef11e8d2695c732c6c618f5f6
wwagner4 <wwagner4@gmail.com>
2016-09-20 23:07:22 +0200

    something to hear on delay

--- doctuswebaudiotryout-core/src/main/scala/net/entelijan/experiments/DelayExperiment.scala
+++ doctuswebaudiotryout-core/src/main/scala/net/entelijan/experiments/DelayExperiment.scala
@@ -4,13 +4,17 @@ import net.entelijan.SoundExperiment
 import net.entelijan.Nineth
 import doctus.sound.DoctusSoundAudioContext
 import doctus.sound.StartStoppable
+import doctus.sound.WaveType_Sine
+import doctus.sound.NodeSource
+import doctus.sound.NodeThrough
+import doctus.sound.NodeSink
 
 case class DelayExperiment(ctx: DoctusSoundAudioContext) extends SoundExperiment {
 
   def title: String = "delay"
-  
+
   var instOpt = Option.empty[Inst]
-  
+
   def start(nineth: Nineth): Unit = {
     val now = ctx.currentTime
     val inst = Inst()
@@ -23,16 +27,56 @@ case class DelayExperiment(ctx: DoctusSoundAudioContext) extends SoundExperiment
     instOpt.foreach { _.stop(now) }
   }
 
+  type N = NodeSource with StartStoppable
+
   case class Inst() extends StartStoppable {
-    
+
+    def createSource(gainVal: Double): N = {
+
+      val gain = ctx.createNodeThroughGain
+      val gainCtrl = ctx.createNodeControlConstant(gainVal)
+
+      val oscil = ctx.createNodeSourceOscil(WaveType_Sine)
+      
+      gainCtrl >- gain.gain
+      
+      oscil >- gain
+
+      new NodeSource with StartStoppable {
+
+        def connect(sink: NodeSink): Unit = gain.connect(sink)
+
+        def connect(through: NodeThrough): NodeSource = gain.connect(through)
+
+        def start(time: Double): Unit = oscil.start(time)
+
+        def stop(time: Double): Unit = oscil.stop(time)
+
+      }
+
+    }
+
+    val sources = List(0.01, 0.01, 0.01).map { gain => createSource(gain) }
+    val sink = ctx.createNodeSinkLineOut
+
+    sources.foreach { src =>
+      src >- sink
+    }
+
     def start(time: Double): Unit = {
-      println("INST started at %.2f" format time)
+      sources.foreach { src =>
+        src.start(time)
+        println("INST started at %.2f" format time)
+      }
     }
 
     def stop(time: Double): Unit = {
-      println("INST stopped at %.2f" format time)
+      sources.foreach { src =>
+        src.stop(time)
+        println("INST stopped at %.2f" format time)
+      }
     }
-    
+
   }
 
 }
