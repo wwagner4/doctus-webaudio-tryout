6bacbfe7629fbcd45e2f7e7a2def06bf11e31268
wwagner4 <wwagner4@gmail.com>
2016-09-22 22:32:32 +0200

    delay works fine

--- doctuswebaudiotryout-core/src/main/scala/net/entelijan/experiments/DelayExperiment.scala
+++ doctuswebaudiotryout-core/src/main/scala/net/entelijan/experiments/DelayExperiment.scala
@@ -1,7 +1,6 @@
 package net.entelijan.experiments
 
-import net.entelijan.SoundExperiment
-import net.entelijan.Nineth
+import net.entelijan.{Nineth, SoundExperiment, SoundUtil}
 import doctus.sound.DoctusSoundAudioContext
 import doctus.sound.StartStoppable
 import doctus.sound.WaveType_Sine
@@ -15,9 +14,15 @@ case class DelayExperiment(ctx: DoctusSoundAudioContext) extends SoundExperiment
 
   var instOpt = Option.empty[Inst]
 
+  val attackValues = List(0.0001, 0.1, 0.6)
+  val delayValues = List(0.01, 0.1, 0.2)
+
   def start(nineth: Nineth): Unit = {
+
+    val (a, d) = SoundUtil.xyParams(attackValues, delayValues)(nineth)
+
     val now = ctx.currentTime
-    val inst = Inst()
+    val inst = Inst(a, d)
     inst.start(now)
     instOpt = Some(inst)
   }
@@ -29,7 +34,7 @@ case class DelayExperiment(ctx: DoctusSoundAudioContext) extends SoundExperiment
 
   case class SrcParam(gainVal: Double, freq: Double, delay: Double)
 
-  case class Inst() extends StartStoppable {
+  case class Inst(attack: Double, delayDiff: Double) extends StartStoppable {
 
     def createSource(param: SrcParam): NodeSource with StartStoppable = {
 
@@ -43,7 +48,7 @@ case class DelayExperiment(ctx: DoctusSoundAudioContext) extends SoundExperiment
       val delayCtrl = ctx.createNodeControlConstant(param.delay)
 
       val envel = ctx.createNodeThroughGain
-      val envelCtrl = ctx.createNodeControlAdsr(0.3, 0.0, 1.0, 2.5)
+      val envelCtrl = ctx.createNodeControlAdsr(attack, 0.0, 1.0, 2.5)
 
       delayCtrl >- delay.delay
       gainCtrl >- gain.gain
@@ -73,9 +78,9 @@ case class DelayExperiment(ctx: DoctusSoundAudioContext) extends SoundExperiment
 
     }
 
-    val gainSeq = Stream.iterate(0.07)(x => x * 0.999)
+    val gainSeq = Stream.iterate(0.2)(x => x * 0.999)
     val freqSeq = Stream.iterate(450.0)(x => x * 1.12599)
-    val delaySeq = Stream.iterate(0.0)(x => x + 0.05)
+    val delaySeq = Stream.iterate(0.0)(x => x + delayDiff)
 
     val sources = gainSeq.zip(freqSeq).zip(delaySeq)
       .map { case ((g, f), d) => SrcParam(g, f, d) }
