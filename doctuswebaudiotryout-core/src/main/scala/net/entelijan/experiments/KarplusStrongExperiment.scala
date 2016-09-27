7f7248f2ddc341798fcca05bb0788e63b5b95626
wwagner4 <wwagner4@gmail.com>
2016-09-27 07:58:35 +0200

    burst with master gain

--- doctuswebaudiotryout-core/src/main/scala/net/entelijan/experiments/KarplusStrongExperiment.scala
+++ doctuswebaudiotryout-core/src/main/scala/net/entelijan/experiments/KarplusStrongExperiment.scala
@@ -14,9 +14,25 @@ case class KarplusStrongExperiment(ctx: DoctusSoundAudioContext) extends SoundEx
 
   var instOpt = Option.empty[Instrument]
 
+  val frequencies = Stream.iterate(400.0)(f => f * math.pow(2, 1.0 / 3))
+
+  def frequencyFrom(nineth: Nineth) = nineth match {
+    case N_00 => frequencies(0)
+    case N_01 => frequencies(1)
+    case N_02 => frequencies(2)
+
+    case N_10 => frequencies(3)
+    case N_11 => frequencies(4)
+    case N_12 => frequencies(5)
+
+    case N_20 => frequencies(6)
+    case N_21 => frequencies(7)
+    case N_22 => frequencies(8)
+  }
+
   def start(nineth: Nineth): Unit = {
-    println("started")
-    val inst = Instrument()
+    val frequency = frequencyFrom(nineth)
+    val inst = Instrument(frequency)
 
     val now = ctx.currentTime
     inst.start(now)
@@ -28,113 +44,40 @@ case class KarplusStrongExperiment(ctx: DoctusSoundAudioContext) extends SoundEx
     instOpt.foreach(inst => inst.stop(now))
   }
 
-  case class Instrument() extends StartStoppable {
+  case class Instrument(frequency: Double) extends StartStoppable {
 
+    val noise = ctx.createNodeSourceNoise(NoiseType_Pink)
+    val gain = ctx.createNodeThroughGain
     val sink = ctx.createNodeSinkLineOut
-    val ks = InstrumentKarplusStrong()
-    val adsr = nodeAdsr
+    val masterGain = createMasterGain(1.0)
 
-    ks >- adsr >- sink
+    val adsr = ctx.createNodeControlAdsr(0.001, 0.0, 1.0, 0.001)
+
+    adsr >- gain.gain
+
+    noise >- gain >- masterGain >- sink
 
     def start(time: Double): Unit = {
+      noise.start(0.0)
       adsr.start(time)
-      ks.start(time)
+      val dur = 0.01
+      noise.stop(time + dur + 3.0)
+      adsr.stop(time + dur)
     }
 
     def stop(time: Double): Unit = {
-      adsr.stop(time)
-      ks.stop(time)
-    }
-
-    // Reusable ???
-    def nodeAdsr: NodeThrough with StartStoppable = {
-      val gain = ctx.createNodeThroughGain
-      val adsr = ctx.createNodeControlAdsr(0.01, 0.0, 1.0, 0.1)
-
-      adsr >- gain.gain
-
-      new NodeThroughContainer  with StartStoppable {
-
-        def start(time: Double): Unit = {
-          adsr.start(time)
-        }
-
-        def stop(time: Double): Unit = {
-          println("adsr stop %.2f" format time)
-          adsr.stop(time)
-        }
-
-        def source: NodeSource = gain
-
-        def sink: NodeSink = gain
-      }
-    }
-
-  }
-
-  // Reusable ???
-  case class InstrumentKarplusStrong() extends NodeSource with StartStoppable {
-
-    val frequency = 440.0 // Herz
-
-    val nodeNoise = ctx.createNodeSourceNoise(NoiseType_White)
-    val nodeGain = ctx.createNodeThroughGain
-    val nodeDelay = delayNode(0.01)
-    val nodeFilter = lowpassNode(frequency)
-    val nodeAttenuation = attenuationNode(0.6)
-
-    nodeNoise >- nodeGain >- nodeDelay >- nodeFilter >- nodeAttenuation >- nodeGain
-
-    def connect(sink: NodeSink): Unit = {
-      nodeGain.connect(sink)
-    }
-
-    def connect(through: NodeThrough): NodeSource = {
-      nodeGain.connect(through)
-    }
-
-    def start(time: Double): Unit = {
-      nodeNoise.start(time)
-      val duration = 0.01
-      println("dur:" + duration)
-      nodeNoise.stop(time + duration )
-      println("KS started")
-    }
-
-    def stop(time: Double): Unit = {
-      println("KS stopped")
       // Nothing to do
     }
 
   }
 
-  def delayNode(delay: Double): NodeThrough = {
+  def createMasterGain(gainVal: Double): NodeThrough = {
+    val gain = ctx.createNodeThroughGain
+    val gainCtrl = ctx.createNodeControlConstant(gainVal)
 
-    val nodeConst = ctx.createNodeControlConstant(delay)
-    val nodeDelay = ctx.createNodeThroughDelay
+    gainCtrl >- gain.gain
 
-    nodeConst >- nodeDelay.delay
-
-    nodeDelay
+    gain
   }
 
-  def lowpassNode(frequency: Double): NodeThrough = {
-
-    val nodeConst = ctx.createNodeControlConstant(frequency)
-    val nodeFilter = ctx.createNodeThroughFilter(FilterType_Lowpass)
-
-    nodeConst >- nodeFilter.frequency
-
-    nodeFilter
-  }
-
-  def attenuationNode(attenuation: Double): NodeThrough = {
-
-    val nodeConst = ctx.createNodeControlConstant(attenuation)
-    val nodeGain = ctx.createNodeThroughGain
-
-    nodeConst >- nodeGain.gain
-
-    nodeGain
-  }
 }
