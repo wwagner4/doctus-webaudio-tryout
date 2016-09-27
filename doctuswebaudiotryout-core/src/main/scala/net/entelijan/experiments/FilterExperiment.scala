9163ac4902fddb760d01e2f3c0f0dde9dcc827b2
wwagner4 <wwagner4@gmail.com>
2016-09-27 14:10:50 +0200

    finished new filter experiment

--- doctuswebaudiotryout-core/src/main/scala/net/entelijan/experiments/FilterExperiment.scala
+++ doctuswebaudiotryout-core/src/main/scala/net/entelijan/experiments/FilterExperiment.scala
@@ -10,18 +10,57 @@ case class FilterExperiment(ctx: DoctusSoundAudioContext) extends SoundExperimen
 
   def title: String = "filter"
 
-  val frequencies = List(100.0, 400.0, 700.0)
+  val frequencies = List(300.0, 600.0, 900.0)
   val types: List[FilterType] = List(FilterType_Lowpass, FilterType_Highpass, FilterType_Bandpass)
+
+  var instOpt = Option.empty[Instrument]
 
   def start(nineth: Nineth): Unit = {
     val (f, t) = SoundUtil.xyParams(frequencies, types)(nineth)
-    println("started %.2f %s" format(f, t))
 
+    val inst = new Instrument(f, t)
+    val now = ctx.currentTime
 
+    inst.start(now)
+    instOpt = Some(inst)
   }
 
   def stop(): Unit = {
-    println("stopped")
+    val now = ctx.currentTime
+    instOpt.foreach(inst => inst.stop(now))
+  }
+
+  case class Instrument(frequency: Double, filterType: FilterType) extends StartStoppable {
+
+    val noise = ctx.createNodeSourceNoise(NoiseType_White)
+
+    val filter = createFilter(frequency, 3, filterType)
+    val sink = ctx.createNodeSinkLineOut
+
+    noise >- filter >- sink
+
+    def start(time: Double): Unit = {
+      noise.start(time)
+      println("started %.2f %s" format(frequency, filterType))
+    }
+
+    def stop(time: Double): Unit = {
+      noise.stop(time)
+      println("stopped")
+    }
+
+    def createFilter(frequency: Double, quality: Double, filterType: FilterType): NodeThroughFilter = {
+
+      val filter = ctx.createNodeThroughFilter(filterType)
+      val ctrlFreq = ctx.createNodeControlConstant(frequency)
+      val ctrlQ = ctx.createNodeControlConstant(quality)
+
+      ctrlFreq >- filter.frequency
+      ctrlQ >- filter.quality
+
+      filter
+    }
 
   }
+
 }
