20bb9a67985af22d3becfb4a939b4a28eaf02791
wwagner4 <wwagner4@gmail.com>
2016-10-10 08:19:58 +0200

    extended log tryout

--- doctuswebaudiotryout-jvm/src/test/scala/net/entelijan/LogTryout.scala
+++ doctuswebaudiotryout-jvm/src/test/scala/net/entelijan/LogTryout.scala
@@ -4,12 +4,31 @@ package net.entelijan
   * Created by wwagner4 on 10/10/16.
   */
 object LogTryout extends App {
+
   val t = 4.0
-  val d = 0.00001
+  val d = 0.0001
 
-  val a = math.pow(math.E, - math.log(d) / t)
 
-  val r = (0.0 to (t, 0.2)).map(x => (x, math.pow(a, - x)))
+  val f = FunctionFactory.logDecent(t, d)
+  val r = (0.0 to (t + 0.5, 0.2)).map(x => (x, f(x)))
 
-  r.foreach{case (x, y) => println(f"$x%10.2f -> $y%5.7f")}
+
+  r.foreach{case (x, y) => println(f"$x%10.2f -> $y%11.7f")}
+}
+
+object FunctionFactory {
+
+  def logDecent(t: Double, d: Double): Double => Double = {
+
+    val a = math.pow(math.E, - math.log(d) / t)
+
+    (x: Double) => {
+      if (x < t) math.pow(a, - x) - x * d / t
+      else 0.0
+    }
+
+  }
+
+
+
 }
