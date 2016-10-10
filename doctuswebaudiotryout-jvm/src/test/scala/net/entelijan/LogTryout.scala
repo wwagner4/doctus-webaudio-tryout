a30b76e2f6b05452a00fbcd45fea3497d6c1bcb8
wwagner4 <wwagner4@gmail.com>
2016-10-10 22:01:19 +0200

    log works ok

--- doctuswebaudiotryout-jvm/src/test/scala/net/entelijan/LogTryout.scala
+++ doctuswebaudiotryout-jvm/src/test/scala/net/entelijan/LogTryout.scala
@@ -1,37 +1,44 @@
 package net.entelijan
 
+import java.text.DecimalFormat
+
 /**
   * Created by wwagner4 on 10/10/16.
   */
 object LogTryout extends App {
 
-  val t = 4.0
-  val d = 0.000001
-  val start = 2.0
-  val stop = 1.0
+  val t = 3.0
+  val start = 5.0
+  val stop = -1.0
 
 
-  val f = FunctionFactory.logDecent(start, stop, t, d)
-  val r = (0.0 to (t + 0.5, 0.2)).map(x => (x, f(x)))
+  val f = FunctionFactory.lin(start, stop, t)
+  val r = (0.0 to(t + 0.5, 0.2)).map(x => (x, f(x)))
 
-
-  r.foreach{case (x, y) => println(f"$x%10.2f -> $y%11.7f")}
+  r.foreach { case (x, y) => println(f"$x%.6f;$y%.6f") }
 }
 
 object FunctionFactory {
 
-  def logDecent(start: Double, stop: Double, time: Double, rest: Double): Double => Double = {
+  def log(start: Double, stop: Double, time: Double): Double => Double = {
 
-    val a = math.pow(math.E, - math.log(rest) / time)
+    val rest = math.abs(start - stop) / 2000.0
+    val a = math.pow(math.E, -math.log(rest) / time)
     val d = start - stop
 
-    (x: Double) => {
-      if (x < time) d * math.pow(a, - x) - x * rest / time + stop
+    x => {
+      if (x < time) d * math.pow(a, -x) - x * rest / time + stop
       else stop
     }
 
   }
 
+  def lin(start: Double, stop: Double, time: Double): Double => Double = {
 
+    x => {
+      if (x < time) start
+      else stop
+    }
 
+  }
 }
