29163dc65e18b95e0f1861f8e131209f834c4317
wwagner4 <wwagner4@gmail.com>
2016-10-10 08:26:38 +0200

    added start / stop value

--- doctuswebaudiotryout-jvm/src/test/scala/net/entelijan/LogTryout.scala
+++ doctuswebaudiotryout-jvm/src/test/scala/net/entelijan/LogTryout.scala
@@ -6,10 +6,12 @@ package net.entelijan
 object LogTryout extends App {
 
   val t = 4.0
-  val d = 0.0001
+  val d = 0.000001
+  val start = 2.0
+  val stop = 1.0
 
 
-  val f = FunctionFactory.logDecent(t, d)
+  val f = FunctionFactory.logDecent(start, stop, t, d)
   val r = (0.0 to (t + 0.5, 0.2)).map(x => (x, f(x)))
 
 
@@ -18,13 +20,14 @@ object LogTryout extends App {
 
 object FunctionFactory {
 
-  def logDecent(t: Double, d: Double): Double => Double = {
+  def logDecent(start: Double, stop: Double, time: Double, rest: Double): Double => Double = {
 
-    val a = math.pow(math.E, - math.log(d) / t)
+    val a = math.pow(math.E, - math.log(rest) / time)
+    val d = start - stop
 
     (x: Double) => {
-      if (x < t) math.pow(a, - x) - x * d / t
-      else 0.0
+      if (x < time) d * math.pow(a, - x) - x * rest / time + stop
+      else stop
     }
 
   }
