84b3dc017385a156a8514c0aa68ae7e860fc57bf
wwagner4 <wwagner4@gmail.com>
2016-10-23 13:17:05 +0200

    line impl callable

--- doctuswebaudiotryout-jvm/src/main/scala/doctus/sound/NodeControlAdsrJvmMinim.scala
+++ doctuswebaudiotryout-jvm/src/main/scala/doctus/sound/NodeControlAdsrJvmMinim.scala
@@ -1,23 +1,20 @@
 package doctus.sound
 
-import akka.actor.{ Actor, ActorRef, Props }
+import akka.actor.{Actor, ActorRef, Props}
+import ddf.minim.UGen
 import ddf.minim.ugens.Constant
+
 import scala.concurrent.duration._
 
 case class NodeControlAdsrJvmMinim(attack: Double, decay: Double, sustain: Double, release: Double, gain: Double, trend: Trend)(ctx: MinimContext) extends NodeControlEnvelope {
 
-  private val minimConst = new Constant(MinimConstants.zero.toFloat)
-
   private val adsrParams = AdsrParams(attack, decay, sustain, release, gain, trend)
 
-  var adsrActor: ActorRef = ctx.actorSystem.actorOf(AdsrActor.props(minimConst, adsrParams))
+  var ugenInput = Option.empty[UGen#UGenInput]
 
-  val f = () => {
-    adsrActor ! AdsrTimeEvent(ctx.currentTime)
-  }
+  var ugen = Option.empty[UGen]
 
-  println("scheduler " + MinimConstants.rateDur)
-  val scheduler = ctx.actorSystem.scheduler.schedule(0.second, MinimConstants.rateDur)(f())(ctx.actorSystem.dispatcher)
+  var adsrActor: ActorRef = ctx.actorSystem.actorOf(AdsrActor.props(ugen, ugenInput, adsrParams))
 
   /**
    * Starts the attack of the ADSR
@@ -46,9 +43,13 @@ case class NodeControlAdsrJvmMinim(attack: Double, decay: Double, sustain: Doubl
   def connect(param: ControlParam): Unit = {
     param match {
       case p: UGenInputAware =>
-        println(s"connecting ADSR $minimConst to ${p.uGenInput} ($param)")
-        minimConst.patch(p.uGenInput)
-        ()
+        ugenInput = Some(p.uGenInput)
+        val _ugen = new Constant(0.0f)
+        ugen = Some(_ugen)
+        _ugen.patch(p.uGenInput)
+        ugenInput = Some(p.uGenInput)
+        println(s"connecting ADSR ${_ugen} to ${p.uGenInput} ($param)")
+
       case _ =>
         throw new IllegalArgumentException(
           s"cannot connect $this to $param. $param is not 'UGenAware'")
@@ -56,7 +57,10 @@ case class NodeControlAdsrJvmMinim(attack: Double, decay: Double, sustain: Doubl
   }
 }
 
-case class AdsrTimeEvent(time: Double)
+
+case object AdsrStartDecayEvent
+
+case object AdsrStartSustainEvent
 
 case object AdsrStartEvent
 
@@ -64,7 +68,7 @@ case object AdsrStopEvent
 
 object AdsrActor {
 
-  def props(minimConst: Constant, adsrParams: AdsrParams): Props = Props(classOf[AdsrActor], minimConst, adsrParams)
+  def props(ugen: Option[UGen], ugenInput: Option[UGen#UGenInput], adsrParams: AdsrParams): Props = Props(classOf[AdsrActor], ugen, ugenInput, adsrParams)
 
 }
 
@@ -72,41 +76,21 @@ case class AdsrParams(attack: Double, decay: Double, sustain: Double, release: D
 
 case class AdsrMsg(time: Double)
 
-class AdsrActor(minimConst: Constant, adsrParams: AdsrParams) extends Actor {
+class AdsrActor(ugen: Option[UGen], ugenInput: Option[UGen#UGenInput],adsrParams: AdsrParams) extends Actor {
   
-  def constValue(v: Double) {
-    minimConst.setConstant(v.toFloat)
-  }
-
-  var cnt = 0
-
-  var value = MinimConstants.zero
-  var diffValue = 0.0
-
-  var time: Double = 0.0
-  var targetTime: Double = 0.0
-
-  val gainDb = adsrParams.gain
-  val sustainDb = adsrParams.gain * adsrParams.sustain
-
   def receive: Receive = {
-    case AdsrTimeEvent(sysTime) =>
-      cnt += 1
-      //println(f"E;$value%.3f;$sysTime%.3f")
 
     case AdsrStartEvent =>
-      time = 0
-      targetTime = (adsrParams.attack / MinimConstants.rateDurSeconds)
-      diffValue = if (targetTime > 0) (gainDb - value) / targetTime else 0.0
-      println(f"[receive] ADSR start event $value%.3f rateSeconds:${MinimConstants.rateDurSeconds}%.3f attack:${adsrParams.attack}%.3f diff:$diffValue%.4f targetTime:$targetTime")
-      context.become(attack)
+      println(f"[receive] AdsrStartEvent")
 
     case AdsrStopEvent =>
-      println(f"[receive] ADSR stop event $value%.3f")
-      time = 0
-      targetTime = (adsrParams.release / MinimConstants.rateDurSeconds)
-      diffValue = if (targetTime > 0) (MinimConstants.zero - value) / targetTime else 0.0
-      context.become(release)
+      println(f"[receive] AdsrStopEvent")
+
+    case AdsrStartDecayEvent =>
+      println(f"[receive] AdsrStartDecayEvent")
+
+    case AdsrStartSustainEvent =>
+      println(f"[receive] AdsrStartSustainEvent")
 
     case msg: Any =>
       println(s"[receive] ADSR unhandled $msg")
@@ -114,107 +98,60 @@ class AdsrActor(minimConst: Constant, adsrParams: AdsrParams) extends Actor {
   }
 
   def attack: Receive = {
-    case AdsrTimeEvent(sysTime) =>
-      time += 1
-      cnt += 1
-      //println(f"A;$value%.3f;$sysTime%.3f")
-      if (time > targetTime) {
-        time = 0
-        if (adsrParams.decay <= 0.0) {
-          if (adsrParams.release <= 0.0) {
-            value = MinimConstants.zero
-            constValue(value)
-            context.become(receive)
-          } else {
-            context.become(receive)
-          }
-        } else {
-          value = gainDb
-          constValue(value)
-          targetTime = (adsrParams.decay / MinimConstants.rateDurSeconds)
-          diffValue = if (targetTime > 0) (sustainDb - value) / targetTime else 0.0
-          println(f"become decay sdb:$sustainDb%.3f sdb:$value%.3f  tt:$targetTime dv:$diffValue%.3f v:$value%.3f")
-          context.become(decay)
-        }
-      } else {
-        value += diffValue
-        constValue(value.toFloat)
-      }
+
+    case AdsrStartEvent =>
+      println(f"[attack] AdsrStartEvent")
 
     case AdsrStopEvent =>
-      println(f"[attack] ADSR stop event $value%.3f")
-      time = 0
-      targetTime = (adsrParams.release / MinimConstants.rateDurSeconds)
-      diffValue = if (targetTime > 0) (MinimConstants.zero - value) / targetTime else 0.0
-      context.become(release)
+      println(f"[attack] AdsrStopEvent")
+
+    case AdsrStartDecayEvent =>
+      println(f"[attack] AdsrStartDecayEvent")
+
+    case AdsrStartSustainEvent =>
+      println(f"[attack] AdsrStartSustainEvent")
 
     case msg: Any =>
-      println(f"[attack] ADSR unhandled $msg")
+      println(s"[attack] ADSR unhandled $msg")
       this.unhandled(msg)
-
   }
 
   def decay: Receive = {
-    case AdsrTimeEvent(sysTime) =>
-      time += 1
-      cnt += 1
-      //println(f"D;$value%.3f;$sysTime%.3f")
-      if (time > targetTime) {
-        println(f"[decay] become 'receive'")
-        value = sustainDb
-        constValue(value.toFloat)
-        context.become(receive)
-      } else {
-        value += diffValue
-        constValue(value)
-      }
 
     case AdsrStartEvent =>
-      println(f"[decay] ADSR start event $value%.3f")
-      time = 0
-      targetTime = (adsrParams.attack / MinimConstants.rateDurSeconds)
-      diffValue = if (targetTime > 0) (gainDb - value) / targetTime else 0.0
-      context.become(attack)
+      println(f"[decay] AdsrStartEvent")
 
     case AdsrStopEvent =>
-      println(f"[decay] ADSR stop event $value%.3f")
-      time = 0
-      targetTime = (adsrParams.release / MinimConstants.rateDurSeconds)
-      diffValue = if (targetTime > 0) (MinimConstants.zero - value) / targetTime else 0.0
-      context.become(release)
+      println(f"[decay] AdsrStopEvent")
+
+    case AdsrStartDecayEvent =>
+      println(f"[decay] AdsrStartDecayEvent")
+
+    case AdsrStartSustainEvent =>
+      println(f"[decay] AdsrStartSustainEvent")
 
     case msg: Any =>
-      println(f"[decay] ADSR unhandled $msg")
+      println(s"[decay] ADSR unhandled $msg")
       this.unhandled(msg)
-
   }
 
   def release: Receive = {
-    case AdsrTimeEvent(sysTime) =>
-      time += 1
-      cnt += 1
-      //println(f"R;$value%.3f;$sysTime%.3f")
-      if (time > targetTime) {
-        println(f"[release] become 'receive'")
-        value = MinimConstants.zero
-        constValue(value)
-        context.become(receive)
-      } else {
-        value += diffValue
-        constValue(value)
-      }
 
     case AdsrStartEvent =>
-      println(f"[release] ADSR start event $value%.3f")
-      time = 0
-      targetTime = (adsrParams.attack / MinimConstants.rateDurSeconds)
-      diffValue = if (targetTime > 0) (gainDb - value) / targetTime else 0.0
-      context.become(attack)
+      println(f"[release] AdsrStartEvent")
+
+    case AdsrStopEvent =>
+      println(f"[release] AdsrStopEvent")
+
+    case AdsrStartDecayEvent =>
+      println(f"[release] AdsrStartDecayEvent")
+
+    case AdsrStartSustainEvent =>
+      println(f"[release] AdsrStartSustainEvent")
 
     case msg: Any =>
       println(s"[release] ADSR unhandled $msg")
       this.unhandled(msg)
-
   }
 
 }
